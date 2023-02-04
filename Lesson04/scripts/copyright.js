360ba932d8bf0dddf73799fc8d2415c41a29a476
AndrewJHalisky <ajhalisky@gmail.com>
2023-02-04 14:34:58 -0500

    Modified the copyright year

---
+++ Lesson04/scripts/copyright.js
@@ -0,0 +1,8 @@
+let date = new Date();
+console.log(date.getFullYear());
+
+
+const currentDate = new Date();
+
+document.querySelector('#year').textContent = currentDate.getFullYear();
+document.getElementById("copyrightYear").innerHTML = copyrightFooter;
