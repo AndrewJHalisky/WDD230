68ceef70e6869408212d581325cdef7b4c607324
AndrewJHalisky <ajhalisky@gmail.com>
2023-03-11 20:55:25 -0500

    Multiple changes made and addons for project.

--- Lesson05/scripts/copyright-dis.js
+++ Lesson05/scripts/copyright-dis.js
@@ -14,7 +14,18 @@ const fulldate = new Intl.DateTimeFormat("en-UK", {
     dateStyle: "full"
 }).format(currentDate);
 
+const visitsDisplay = document.querySelector("#visits")
 
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+if (numVisits !=0){
+    visitsDisplay.textContent = numVisits;
+    } else {
+    visitsDisplay.textContent = `This is your first visit!`;
+}
+numVisits++
+localStorage.setItem("visits-ls", numVisits)
+todayDisplay.textContent = Date.now();
 
 document.querySelector('#date').textContent = fulldate;
 document.querySelector('#year').textContent = currentDate.getFullYear();
--- Lesson05/scripts/join.js
+++ Lesson05/scripts/join.js
@@ -1,7 +1,17 @@
 const re = new RegExp("ab+c");
 
 const firstName = document.querySelector('form[name="first_name"]');
+const lastName = document.querySelector('form[name="last_name"]');
+const userEmail = document.querySelector('form[name="email"]');
+const phoneNumber = document.querySelector('form[name="phone_number"]');
+const userBusiness = document.querySelector('form[name="business"]');
+const memberLevel = document.querySelector('form[name="membership"]');
 
 function join() {
+    console.log('first_name');
+    console.log('last_name');
     console.log('email');
+    console.log('phone_number');
+    console.log('business');
+    console.log('membership');
 }
