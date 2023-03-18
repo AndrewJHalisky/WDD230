168955db259701b7e009b2db07b6dc045db842fb
AndrewJHalisky <ajhalisky@gmail.com>
2023-03-18 13:21:14 -0400

    Made corrections once again.

--- Lesson05/scripts/copyright-dir.js
+++ Lesson05/scripts/copyright-dir.js
@@ -13,3 +13,6 @@ const currentDate = new Date();
 const fulldate = new Intl.DateTimeFormat("en-UK", {
     dateStyle: "full"
 }).format(currentDate);
+
+document.querySelector('#date').textContent = fulldate;
+document.querySelector('#year').textContent = currentDate.getFullYear();
