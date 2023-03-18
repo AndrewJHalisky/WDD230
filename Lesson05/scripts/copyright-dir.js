b0a846f4fa8e147d86359b9ed82a0e2858fbe6eb
AndrewJHalisky <ajhalisky@gmail.com>
2023-03-18 13:18:54 -0400

    Added directory javascript

---
+++ Lesson05/scripts/copyright-dir.js
@@ -0,0 +1,15 @@
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const mainnav = document.querySelector('.primaryNav');
+const hambutton = document.querySelector('#menu');
+
+document.write("Last modified on" + " " +document.lastModified);
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('open')}, false);
+ 
+const currentDate = new Date();
+
+const fulldate = new Intl.DateTimeFormat("en-UK", {
+    dateStyle: "full"
+}).format(currentDate);
