76564f047ef103c29198b697cb68b1c7896c821b
AndrewJHalisky <ajhalisky@gmail.com>
2023-09-29 20:23:33 -0400

    Finished updated home page.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,9 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation')
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
