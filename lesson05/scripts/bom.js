cf3595c635071535fb31383387091a4cc5326cf6
AndrewJHalisky <ajhalisky@gmail.com>
2023-10-11 14:18:09 -0400

    Fixed error with program.

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,25 +1,22 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-button.addEventListener('click', function () {
+button.addEventListener('click', function() {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
     if (input.value != '') {
         li.textContent = input.value;
         deleteButton.textContent = '❌';
         li.append(deleteButton);
         li.append(input.textContent)
         list.append(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus(deleteButton);
+        })
     }
 });
-
-deleteButton.addEventListener('click', function () {
-    list.removeChild(li);
-    input.focus(deleteButton);
-});
-
-input.focus();
-input.value = '';
 
 // button.addEventListener('click', () => { ... });
