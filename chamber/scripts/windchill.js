e758a7dcc0c5158dac9fe1800a6a7610830a2e59
AndrewJHalisky <ajhalisky@gmail.com>
2023-10-21 14:26:11 -0400

    Fixed the windchill for proper code.

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,13 +1,9 @@
-var temp = 60
-var wSpeed = 17
+let temp = 60;
+let wSpeed = 17;
 
-if (temp <= 50, wSpeed > 3) {
-    var windChill = (35.7 + (0.6215 * temp)) - (35.75 * Math.pow(wSpeed, 0.16)) + (0.4275 * temp * Math.pow(wSpeed, 0.16));
-    var windChill = Math.round(windChill);
+if (temp <= 50 && wSpeed > 3) {
+    let windChill = (35.7 + (0.6215 * temp)) - (35.75 * Math.pow(wSpeed, 0.16)) + (0.4275 * temp * Math.pow(wSpeed, 0.16));
+    windChill = Math.round(windChill);
     document.getElementById("temp").innerHTML = temp;
     document.getElementById("windChill").innerHTML = windChill;
-}
-
-else {
-    console.log("N/A");
 }
