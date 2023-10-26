6de8d69e2438ec13d2f77bc58164daa49aaa92a0
AndrewJHalisky <ajhalisky@gmail.com>
2023-10-26 16:55:45 -0400

    Finished enchacement. First attempt.

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,9 +1,13 @@
-let temp = 60;
-let wSpeed = 17;
+let temp = parseFloat(document.querySelector('#temp').textContent);
+let wSpeed = parseFloat(document.querySelector('#wSpeed').textContent);
+document.getElementById("temp").innerHTML = temp;
+document.getElementById("windChill").innerHTML = windChill;
 
-if (temp <= 50 && wSpeed > 3) {
+if (temp <= 50, wSpeed > 3) {
     let windChill = (35.7 + (0.6215 * temp)) - (35.75 * Math.pow(wSpeed, 0.16)) + (0.4275 * temp * Math.pow(wSpeed, 0.16));
+    document.querySelector("#windChill").innerHTML = windChill;
     windChill = Math.round(windChill);
-    document.getElementById("temp").innerHTML = temp;
-    document.getElementById("windChill").innerHTML = windChill;
+}
+else {
+    windChill = "N/A";
 }
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -7,3 +7,19 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+const visitsDisplay = document.querySelector('.visits');
+
+// getItem loads the data or the times the user visited the website.
+// setItem saves the data or the times the user visited the website.
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+}
+else {
+    visitsDisplay.textContent = `Welcome! This is your first time visiting the home page.`;
+}
+// ++ increases the number of times the website was visited.
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
