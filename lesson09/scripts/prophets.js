e8a421f021f3e874f929b598149bfb3831558cfa
AndrewJHalisky <ajhalisky@gmail.com>
2023-11-10 19:46:20 -0500

    Completed Prophets JSON

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -2,11 +2,34 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 const cards = document.querySelector('#cards');
 
 const displayProphets = (prophets) => {
-    async function getProphetData() {
-        const response = await fetch(url);
-        const data = await response.json();
-        // console.table(data.prophets);
-        displayProphets(data.prophets);
-    }
-    getProphetData();
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        cards.appendChild(card);
+    });
 }
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+getProphetData();
