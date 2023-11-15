2fc1e722206af759a4f3291b989e23f2d9e24dcc
AndrewJHalisky <ajhalisky@gmail.com>
2023-11-15 14:54:05 -0500

    Pushing for images.

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,12 +1,44 @@
-const url = "https://andrewjhalisky.github.io/wdd230/chamber/data/members.json"
+const url = "https://andrewjhalisky.github.io/wdd230/chamber/data/members.json";
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
 
-const displayLinks = (businesses) => {
-    businesses.forEach((business))
+const displayLinks = businesses => {
+    businesses.forEach(business => {
+        let card = document.createElement('section');
+        let busiTitle = document.createElement('h3');
+        let website = document.createElement('a');
+        let image = document.createElement('img');
+        let info = document.createElement('p');
+        let membLevel = document.createElement('p');
+        let desc = document.createElement('p');
+
+        busiTitle.textContent = `${business.name}`;
+        image.setAttribute('src', business.image);
+        image.setAttribute('alt', busiTitle);
+        image.setAttribute('loading', 'lazy');
+        
+        website.textContent = `${business.websiteurl}`;
+        info.textContent = `${business.address} ${business.phonenumber}`;
+        membLevel.textContent = `${business.membershiplevel}`;
+        desc.textContent = `${business.description}`;
+
+        card.appendChild(busiTitle);
+        card.appendChild(image);
+        card.appendChild(website);
+        card.appendChild(info);
+        card.appendChild(membLevel);
+        card.appendChild(desc);
+        display.appendChild(card);
+    });
 }
 
-async function getLinks(){
+async function getBusinesses() {
     const response = await fetch(url);
     const data = await response.json();
-    displayLinks(data.lessons);
+    console.log(data.businesses)
+    displayLinks(data.businesses);
 };
-getLinks();
+
+
+getBusinesses();
