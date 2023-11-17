1223feae7d50f22301788b45078c106c8834c2a4
AndrewJHalisky <ajhalisky@gmail.com>
2023-11-16 20:09:55 -0500

    Changed Farenheit format.

--- scripts/curweather.js
+++ scripts/curweather.js
@@ -1,7 +1,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75168&lon=6.663573&&units=imperial&appid=b53e334657077c85a2b7c51973b22f75';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.17921&lon=-75.04345&&units=imperial&appid=b53e334657077c85a2b7c51973b22f75';
 
 async function apiFetch(){
     try{
@@ -23,8 +23,7 @@ apiFetch();
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp} &deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
-
+    let desc = data.weather[0].description.toUpperCase();
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = (desc);
